462b6f79a7d65d04708bc13b742716de0ef85bc6
Shivanshu Gupta <shivanshu.gupta@gmail.com>
2024-10-26 17:23:15 -0700

    cr graph.py comments

--- src/graph.py
+++ src/graph.py
@@ -15,7 +15,8 @@ from data_classes import validate_output_against_schema
 from cust_logger import logger, set_files_message_color
 from datetime import datetime
 
-set_files_message_color('MAGENTA')
+set_files_message_color('MAGENTA') # file specific JSON logging of type
+                                   # {"timestamp": "", "uuid": "", "msg": "", "data": ""}
 
 # loads and checks if env var exists before continuing to model invocation
 load_dotenv()
@@ -40,22 +41,30 @@ except Exception as e:
     logger.fatal(f"Fatal Error: Failed to initialize model: {e}")
     sys.exit(1)
 
+# this is LangGraph's internal state, currently we are using it to manage error handling and internal history through the graph
 class GraphsState(TypedDict):
     internal_hist: Annotated[list[str], operator.add]
 
 graph = StateGraph(GraphsState)
 
 def _call_model(state: GraphsState, config: RunnableConfig):
+
+    # extract data from respective application details to be used within the llm invocation, giving as much detail as it is necessary
     internal_hist = state["internal_hist"]
-    user_msg = internal_hist[0]
-    error_hist = internal_hist[1:]
+    user_msg = internal_hist[0] # entry user message, our "goal" to answer
+    error_hist = internal_hist[1:] # history of validation or other issues the model has generated that it has to fix
     app_data = config["configurable"]["app_details"]
+
+    # extract components used for application details in the prompt
     app_uuid = app_data["uuid"]
     app_prompt = app_data['prompt']
     input_schema_properties = app_data['input_schema']["properties"]
     output_schema_properties = app_data['output_schema']["properties"]
+    # these are the user facing application logs, not the system logs
     app_logs = app_data['app_logs']
 
+    # define a prompt template to be used and filled, so that model will dynamically will be handling issues and requests
+    # if you remove `respond in JSON format, dont add any preamble` and invoke the model you can see the validation work effectively
     template = """
     you do NOT have access to tools or functions, do NOT make calls to tools or functions ever.
 
@@ -101,45 +110,64 @@ def _call_model(state: GraphsState, config: RunnableConfig):
         "app_logs": app_logs
     }
 
+    # current data being used within prompt to give our output
     logger.info({"timestamp": datetime.now().isoformat(), "uuid": app_uuid, "msg": "Calling model with formatted prompt.", "data": log_data})
     response = llm.invoke(final_prompt)
 
+    # add it to our history to be used in future nodes
     return {"internal_hist": [response.content]}
 
 def conditional_check(state: GraphsState, config: RunnableConfig):
+    # this node is meant to be quality control check, checking the validity of the LLM response structure wise and also data wise
+    # if its not we tell ask the model to regenerate it with the JSON or SCHEMA errors we produced
     app_uuid = config["configurable"]["app_details"]["uuid"]
     last_msg = state["internal_hist"][-1]
     logger.info({"timestamp": datetime.now().isoformat(), "uuid": app_uuid, "msg": "Model response received.", "data": last_msg})
+
+    # this inital QC is if the response is in JSON format, as expected
     if isinstance(last_msg, str) and last_msg.startswith('{') and last_msg.endswith('}'):
+        # attempt to parse the JSON and throw errors if there are internal structure errors
         try:
             json_object = json.loads(last_msg)
             app_data = config["configurable"]["app_details"]
-            output_schema = app_data['output_schema']
+            output_schema = app_data['output_schema'] # JSON format of OutputSchema BaseModel
+            # this checks any output SCHEMA errors, the respective app has an expected output that the response needs to conform to
+            # data validity check essentally
             validation_errors = validate_output_against_schema(json_object, output_schema)
             if validation_errors:
                 logger.error({"timestamp": datetime.now().isoformat(), "uuid": app_uuid, "msg": "Schema validation failed with errors", "data": f"{', '.join(validation_errors)}"})
+                # we add it to history to have to model fix in next pass
                 return {"internal_hist": [f"ERROR_SCHEMA - {', '.join(validation_errors)}"]}
+            # everything was sucessfully valid! there are no JSON(structure) or SCHEMA(data) errors
+            # we dont need to add anything to the history, and can just return the state as is
             return state
         except json.JSONDecodeError as e:
+            # even though the model gave a "JSON" string, it could still be malformed structure wise
             logger.error({"timestamp": datetime.now().isoformat(), "uuid": app_uuid, "msg": "JSON decoding failed", "data": f"{e}"})
+            # we add it to history to have to model fix in next pass
             return {"internal_hist": [f"ERROR_JSON - converting string to JSON: {e}"]}
     else:
+        # retry with and give only JSON format
         logger.error({"timestamp": datetime.now().isoformat(), "uuid": app_uuid, "msg": "Invalid response format. Expected JSON.", "data": last_msg})
+        # we add it to history to have to model fix in next pass
         return {"internal_hist": ["ERROR_JSON - Invalid response format. Respond only in JSON starting with `{` and ending with `}`"]}
 
 def should_retry(state: GraphsState, config: RunnableConfig):
+    # this is a conditional "flag" that checks if the model is completed sucessfully or should it retry by checking last message for errors
     app_uuid = config["configurable"]["app_details"]["uuid"]
     last_msg = state["internal_hist"][-1]
+    # im sure theres a better way of handling checking if the previous message is an `ERROR` but this works for now
     if isinstance(last_msg, str) and last_msg.startswith('ERROR'):
         logger.warning({"timestamp": datetime.now().isoformat(), "uuid": app_uuid, "msg": "Error detected in last message, retrying.", "data": ""})
         return True
     logger.info({"timestamp": datetime.now().isoformat(), "uuid": app_uuid, "msg": "response is valid", "data": ""})
     return False
 
-graph.add_node("retry_check", conditional_check)
-graph.add_node("model_node", _call_model)
+# building the graph in the order it should go, model_node -> _call_model -> should_retry? -> END/`_model_node` for retry
 graph.add_edge(START, "model_node")
+graph.add_node("model_node", _call_model)
 graph.add_edge("model_node", "retry_check")
+graph.add_node("retry_check", conditional_check)
 graph.add_conditional_edges(
     "retry_check",
     should_retry,
@@ -152,11 +180,12 @@ graph.add_conditional_edges(
 graph_runnable = graph.compile()
 
 async def ainvoke_our_graph(message: Union[str, dict], app_details: Dict[str, Any]):
-    thread_config = {"configurable": {"app_details": app_details}}
-    print()
+    # invoke our graph asynchronously, convert string to json dict, and then do the LLM majik :)
+    llm_config = {"configurable": {"app_details": app_details}}
     if isinstance(message, dict):
         message = json.dumps(message)
-    messages = await graph_runnable.ainvoke({"internal_hist": [message]}, thread_config)
+    messages = await graph_runnable.ainvoke({"internal_hist": [message]}, llm_config)
+    # we always expect the last message to be the valid and completed "response"
     resp = messages["internal_hist"][-1]
     logger.info({"timestamp": datetime.now().isoformat(), "uuid": app_details["uuid"], "msg": "Graph invocation complete", "data": resp})
     return resp
